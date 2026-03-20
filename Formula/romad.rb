9cb587c37280bd08fe28d45988d0bed7d5f12720
ImmersionOne <axum202@gmail.com>
2026-03-19 20:38:54 -0700

    update Homebrew formula to v0.9.0

--- Formula/romad.rb
+++ Formula/romad.rb
@@ -3,8 +3,8 @@ class Romad < Formula
 
   desc "Travel networking toolkit for digital nomads"
   homepage "https://github.com/ImmersionOne/romad"
-  url "https://github.com/ImmersionOne/romad/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "d78b7d7bcdf973694c454fffca34cceca50dfb857e836340ec17ba8b0be348db"
+  url "https://github.com/ImmersionOne/romad/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "cedf517847443b85bf91fbe0158fb0b5078e79b4fc7c048fa99964e86894528d"
   license "MIT"
 
   depends_on "python@3.12"
